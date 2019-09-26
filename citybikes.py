1df770bd9581c3579605ef4094162bfe0a8418b7
Joonas Saario <jsaario@gmail.com>
2019-09-26 18:03:12 +0300

    Added logic for parsing the query replies.

--- citybikes.py
+++ citybikes.py
@@ -30,8 +30,35 @@ for station in stations:
 	reply_json = reply.json()
 	replies.append(reply_json["data"]["bikeRentalStation"])
 
-# "state": "Station on",
-# "state": "Station off",
+# Parse the replies.
+station_data = {}
+for reply in replies:
+	# Get the values.
+	station_id = reply.get("stationId", None)
+	station_name = reply.get("name", None)
+	station_state = reply.get("state", "")
+	station_bikes = reply.get("bikesAvailable", 0)
+	station_emptyspaces = reply.get("spacesAvailable", 0)
+	station_dropoff = reply.get("allowDropoff", False)
+	# Check the values.
+	if station_id is None or station_name is None:
+		pass
+	# Check the state of the station.
+	if station_state == "Station on" and station_dropoff:
+		station_available = True
+	else:
+		station_available = False
+	# Calculate the station size.
+	station_size = station_bikes + station_emptyspaces
+	# Store the values.
+	station_data.update({station_id: {
+		"name": station_name,
+		"available": station_available,
+		"size": station_size,
+		"bikes": station_bikes
+		}})
+
+# Next: Output.
 
 # All done, exit.
 exit(0)
