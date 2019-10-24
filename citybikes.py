d07a9d7d04dfd42b8bbb92cc00261fb62d52de20
Joonas Saario <jsaario@gmail.com>
2019-10-24 18:44:51 +0300

    Added an improved routine for getting and parsing the station data.

--- citybikes.py
+++ citybikes.py
@@ -16,45 +16,18 @@ from requests import post as http_post
 
 # Functions.
 
-# Queries the HSL API for station information.
-# If station ID is given, extended information for that station is returned.
-# If no ID is given, limited information for all stations is returned.
-def query_stations(station_id=None):
-	# Set the query url. This only needs to be changed if the API changes.
-	url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
-	# Create the output.
-	station_data = []
-	# Check if one station or all stations should be queried.
-	if station_id:
-		# Master keyword used in retrieving the data.
-		keyword = "bikeRentalStation"
-		# Values to be retrieved.
-		values = "stationId name state bikesAvailable spacesAvailable allowDropoff"
-		# String used in the query. The API requires curly spaces in the query string, thus making .format() syntax very, very ugly.
-		query = "{{{keyword}(id: \"{station}\") {{{values}}}}}".format(keyword=keyword, station=station_id, values=values)
-	else:
-		# Master keyword used in retrieving the data.
-		keyword = "bikeRentalStations"
-		# Values to be retrieved.
-		values = "stationId name"
-		# String used in the query. The API requires curly spaces in the query string, thus making .format() syntax very, very ugly.
-		query = "{{{keyword} {{{values}}}}}".format(keyword=keyword, values=values)
-	# Query the API.
-	reply = http_post(url, json={"query": query})
-	# Get the JSON-formatted version of the reply and extract the station information.
-	reply_json = reply.json()
-	station_data = reply_json["data"][keyword]
-	# All done, return.
-	return station_data
-
-
-# Parses the output of query_bikes and returns a list.
-def parse_replies(replies):
+# Returns data for the given stations.
+def get_station_data(stations):
 	# Create the output.
 	station_data = {}
-	# Parse the replies.
-	for reply in replies:
-		# Get the values.
+	# Query the stations one at a time.
+	for station_id in stations:
+		reply = query_stations(station_id=station_id)
+		# Check the contents of the reply and skip empty replies.
+		if not reply:
+			print("Warning: Station '{}' not found.".format(station_id))
+			continue
+		# Get the values from the reply.
 		station_id = reply.get("stationId", None)
 		station_name = reply.get("name", None)
 		station_state = reply.get("state", "")
@@ -123,6 +96,71 @@ def print_output(station_data, hide_empty=False, hide_unavailable=False):
 	# All done, return.
 	return None
 
+# Queries the HSL API for station information.
+# If station ID is given, extended information for that station is returned.
+# If no ID is given, limited information for all stations is returned.
+def query_stations(station_id=None):
+	# Set the query url. This only needs to be changed if the API changes.
+	url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
+	# Check if one station or all stations should be queried.
+	if station_id:
+		# Master keyword used in retrieving the data.
+		keyword = "bikeRentalStation"
+		# Values to be retrieved.
+		values = "stationId name state bikesAvailable spacesAvailable allowDropoff"
+		# String used in the query. The API requires curly spaces in the query string, thus making .format() syntax very, very ugly.
+		query = "{{{keyword}(id: \"{station}\") {{{values}}}}}".format(keyword=keyword, station=station_id, values=values)
+	else:
+		# Master keyword used in retrieving the data.
+		keyword = "bikeRentalStations"
+		# Values to be retrieved.
+		values = "stationId name"
+		# String used in the query. The API requires curly spaces in the query string, thus making .format() syntax very, very ugly.
+		query = "{{{keyword} {{{values}}}}}".format(keyword=keyword, values=values)
+	# Query the API.
+	reply = http_post(url, json={"query": query})
+	# Get the JSON-formatted version of the reply and extract the station information.
+	reply_json = reply.json()
+	reply_data = reply_json["data"][keyword]
+	# All done, return.
+	return reply_data
+
+
+# Parses the output of query_bikes and returns a list.
+def parse_replies(replies):
+	# Create the output.
+	station_data = {}
+	# Parse the replies.
+	for reply in replies:
+		if not reply:
+			continue
+		# Get the values.
+		station_id = reply.get("stationId", None)
+		station_name = reply.get("name", None)
+		station_state = reply.get("state", "")
+		station_bikes = reply.get("bikesAvailable", 0)
+		station_emptyspaces = reply.get("spacesAvailable", 0)
+		station_dropoff = reply.get("allowDropoff", False)
+		# Check the values.
+		if station_id is None or station_name is None:
+			pass
+		# Check the state of the station.
+		if station_state == "Station on" and station_dropoff:
+			station_available = True
+		else:
+			station_available = False
+		# Calculate the station size.
+		station_size = station_bikes + station_emptyspaces
+		# Store the values.
+		station_data.update({station_id: {
+			"name": station_name,
+			"available": station_available,
+			"size": station_size,
+			"bikes": station_bikes
+			}})
+	# All done, return.
+	return station_data
+
 # Queries the given URL for the bikes.
 def query_bikes_old(stations):
 	url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
@@ -162,12 +200,17 @@ arguments = argument_parser.parse_args()
 # Get the list of stations.
 stations = arguments.stations
 
-# Query the HKL API and parse the replies.
+# Query the HKL API and parse the replies, old version.
 query_replies = [query_stations(station_id=station) for station in stations]
-station_data = parse_replies(query_replies)
+station_data_old = parse_replies(query_replies)
+
+# Query the HKL API and parse the replies, new version.
+station_data = get_station_data(stations)
 
 # Print the output.
+print_output(station_data_old, hide_empty=arguments.hide_empty, hide_unavailable=arguments.hide_unavailable)
 print_output(station_data, hide_empty=arguments.hide_empty, hide_unavailable=arguments.hide_unavailable)
+
 
 # All done, exit.
 exit(0)
