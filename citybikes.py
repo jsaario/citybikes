21bc22e44c44b858aa9d5d091b4aded71f7f36c1
Joonas Saario <jsaario@gmail.com>
2019-10-24 18:46:10 +0300

    Removed old routines.

--- citybikes.py
+++ citybikes.py
@@ -126,67 +126,6 @@ def query_stations(station_id=None):
 	return reply_data
 
 
-# Parses the output of query_bikes and returns a list.
-def parse_replies(replies):
-	# Create the output.
-	station_data = {}
-	# Parse the replies.
-	for reply in replies:
-		if not reply:
-			continue
-		# Get the values.
-		station_id = reply.get("stationId", None)
-		station_name = reply.get("name", None)
-		station_state = reply.get("state", "")
-		station_bikes = reply.get("bikesAvailable", 0)
-		station_emptyspaces = reply.get("spacesAvailable", 0)
-		station_dropoff = reply.get("allowDropoff", False)
-		# Check the values.
-		if station_id is None or station_name is None:
-			pass
-		# Check the state of the station.
-		if station_state == "Station on" and station_dropoff:
-			station_available = True
-		else:
-			station_available = False
-		# Calculate the station size.
-		station_size = station_bikes + station_emptyspaces
-		# Store the values.
-		station_data.update({station_id: {
-			"name": station_name,
-			"available": station_available,
-			"size": station_size,
-			"bikes": station_bikes
-			}})
-	# All done, return.
-	return station_data
-
-# Queries the given URL for the bikes.
-def query_bikes_old(stations):
-	url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
-	values = ["stationId", "name", "state", "bikesAvailable", "spacesAvailable", "allowDropoff"]
-	# Create the output.
-	replies = []
-	# Create a string from the query value list.
-	value_string = " ".join(values)
-	# The API only allows to query one station at a time. Let's query one station at a time, then.
-	for station in stations:
-		# Query the station. The API requires curly spaces in the query string, thus making .format() syntax very, very ugly.
-		query_string = "{{bikeRentalStation(id: \"{station}\") {{{values}}}}}".format(station=station, values=value_string)
-		reply = http_post(url, json={"query": query_string})
-		# Get the JSON-formatted version of the reply and extract the station information.
-		reply_json = reply.json()
-		reply_station = reply_json["data"]["bikeRentalStation"]
-		# Check the contents of the reply and skip empty replies.
-		if not reply_station:
-			print("Warning: Station '{}' not found.".format(station))
-			continue
-		# Append the reply.
-		replies.append(reply_station)
-	# All done, return.
-	return replies
-
-
 # Main.
 
 # Create a parser for the command line arguments.
@@ -200,17 +139,11 @@ arguments = argument_parser.parse_args()
 # Get the list of stations.
 stations = arguments.stations
 
-# Query the HKL API and parse the replies, old version.
-query_replies = [query_stations(station_id=station) for station in stations]
-station_data_old = parse_replies(query_replies)
-
-# Query the HKL API and parse the replies, new version.
+# Query the HKL API and parse the replies.
 station_data = get_station_data(stations)
 
 # Print the output.
-print_output(station_data_old, hide_empty=arguments.hide_empty, hide_unavailable=arguments.hide_unavailable)
 print_output(station_data, hide_empty=arguments.hide_empty, hide_unavailable=arguments.hide_unavailable)
-
 
 # All done, exit.
 exit(0)
